574950c53f771ea2935ee673c24c86a751d4cbe7
Alicia Truong <aliciatruong@Alicias-MacBook-Pro.local>
2024-09-29 20:42:55 -0700

    making changes

--- core-services/src/main/java/org/devteam1/util/RandomNameGenerator.java
+++ core-services/src/main/java/org/devteam1/util/RandomNameGenerator.java
@@ -2,19 +2,19 @@ package org.devteam1.util;
 import java.util.Random;
 
 public class RandomNameGenerator {
-    public static String[] ADJ = {
-            "Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Pink", "Black", "Brown", "White"
+    public static final String[] ADJ = {
+            "Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Pink", "Black", "Brown", "White", "Cyan", "Seaform Green", "Sage"
     };
 
-    public static String[] NOUN = {
-            "Elephant", "Lion", "Tiger", "Dolphin", "Gorilla", "Kangaroo", "Bear", "Giraffe", "Zebra", "Rabbit"
+    public static final String[] NOUN = {
+            "Lion", "Elephant", "Dolphin", "Tiger", "Giraffe", "Eagle", "Parrot", "Penguin", "Owl", "Flamingo", "Crocodile", "Iguana", "Snake", "Tortoise", "Chameleon", "Frog", "Salamander", "Newt", "Toad", "Axolotl", "Salmon", "Goldfish", "Shark", "Clownfish", "Betta", "Butterfly", "Ant", "Dragonfly", "Beetle", "Bee", "Javan Leopard", "Javan Gibbon", "Sunda Pangolin", "Sumatran Elephant", "Banteng", "Javan Hawk-Eagle", "Bali Myna", "Javanese Pheasant", "White-throated Kingfisher", "Black-naped Oriole", "Javan Monitor Lizard", "Green Tree Python", "Java Blue Flying Dragon", "Borneo and Java Tortoise", "Java Frog", "Banded Bullfrog", "Common Javanese Toad", "Giant Snakehead", "Atlas Moth", "Common Mormon Butterfly", "Leafcutter Ant"
 
     };
 
     public static String create() {
-        Random rand = new Random();
-        String adj = ADJ[rand.nextInt(ADJ.length)];
-        String noun = NOUN[rand.nextInt(NOUN.length)];
+        final Random rand = new Random();
+        final String adj = ADJ[rand.nextInt(ADJ.length)];
+        final String noun = NOUN[rand.nextInt(NOUN.length)];
         return adj + " " + noun;
     }
 
